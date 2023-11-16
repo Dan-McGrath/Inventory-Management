ddd0f08f1d4168b8074f4e87c9dc8054a6cb9e0a
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-15 22:31:59 -0500

    add virtual index for slug

--- local-resturant/models/category.js
+++ local-resturant/models/category.js
@@ -6,6 +6,12 @@ const CategorySchema = new Schema({
   name: { type: String, required: true, maxLength: 100, minLength: 3 },
 });
 
+CategorySchema.virtual("slug").get(function () {
+  let slug = this.name;
+  slug.replace(" ", "_");
+  return slug;
+});
+
 CategorySchema.virtual("url").get(function () {
   return `/menu/${this._id}`;
 });
