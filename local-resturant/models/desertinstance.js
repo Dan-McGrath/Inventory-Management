d695daadbd3e4a9ca78dd516404743525bb7b112
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-16 01:15:46 -0500

    add virtual index for category slug

--- local-resturant/models/desertinstance.js
+++ local-resturant/models/desertinstance.js
@@ -19,6 +19,12 @@ DesertInstanceSchema.virtual("slug").get(function () {
   return slug;
 });
 
+DesertInstanceSchema.virtual("categorySlug").get(function () {
+  let slug = this.category.name;
+  slug.replace(" ", "_");
+  return slug;
+});
+
 DesertInstanceSchema.virtual("url").get(function () {
   return `/menu/${this.entree.category._id}/desertinstance/${this._id}`;
 });
