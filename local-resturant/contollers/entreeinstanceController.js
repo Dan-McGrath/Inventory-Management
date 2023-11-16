9778098fd45517aeefdb1f95fc50295ba00e22b1
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-16 00:54:47 -0500

    fix typos

--- local-resturant/contollers/entreeinstanceController.js
+++ local-resturant/contollers/entreeinstanceController.js
@@ -1,42 +1,42 @@
 const EntreeInstance = require("../models/entreeInstance");
 const asyncHandler = require("express-async-handler");
 
-// Display list of all DrinkInstances.
-exports.drinkinstance_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: DrinkInstance list");
+// Display list of all EntreeInstances.
+exports.entreeinstance_list = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: EntreeInstance list");
 });
 
-// Display detail page for a specific DrinkInstance.
-exports.drinkinstance_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: DrinkInstance detail: ${req.params.id}`);
+// Display detail page for a specific EntreeInstance.
+exports.entreeinstance_detail = asyncHandler(async (req, res, next) => {
+  res.send(`NOT IMPLEMENTED: EntreeInstance detail: ${req.params.id}`);
 });
 
-// Display DrinkInstance create form on GET.
-exports.drinkinstance_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: DrinkInstance create GET");
+// Display EntreeInstance create form on GET.
+exports.entreeinstance_create_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: EntreeInstance create GET");
 });
 
-// Handle DrinkInstance create on POST.
-exports.drinkinstance_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: DrinkInstance create POST");
+// Handle EntreeInstance create on POST.
+exports.entreeinstance_create_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: EntreeInstance create POST");
 });
 
-// Display DrinkInstance delete form on GET.
-exports.drinkinstance_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: DrinkInstance delete GET");
+// Display EntreeInstance delete form on GET.
+exports.entreeinstance_delete_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: EntreeInstance delete GET");
 });
 
-// Handle DrinkInstance delete on POST.
-exports.drinkinstance_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: DrinkInstance delete POST");
+// Handle EntreeInstance delete on POST.
+exports.entreeinstance_delete_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: EntreeInstance delete POST");
 });
 
-// Display DrinkInstance update form on GET.
-exports.drinkinstance_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: DrinkInstance update GET");
+// Display EntreeInstance update form on GET.
+exports.entreeinstance_update_get = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: EntreeInstance update GET");
 });
 
-// Handle DrinkInstance update on POST.
-exports.drinkinstance_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: DrinkInstance update POST");
+// Handle EntreeInstance update on POST.
+exports.entreeinstance_update_post = asyncHandler(async (req, res, next) => {
+  res.send("NOT IMPLEMENTED: EntreeInstance update POST");
 });
