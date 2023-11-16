1a30c8ae1dcd044035d8ccf7c062b7552c1f4d0d
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-16 16:27:54 -0500

    change url to accept slug

--- local-resturant/models/side.js
+++ local-resturant/models/side.js
@@ -15,14 +15,8 @@ SideSchema.virtual("slug").get(function () {
   return slug;
 });
 
-SideSchema.virtual("categorySlug").get(function () {
-  let slug = this.category.name;
-  slug.replace(" ", "_");
-  return slug;
-});
-
 SideSchema.virtual("url").get(function () {
-  return `/menu/${this.category._id}/${this._id}`;
+  return `/menu/${this.category.slug}/${this.slug}`;
 });
 
 module.exports = mongoose.model("Side", SideSchema);
