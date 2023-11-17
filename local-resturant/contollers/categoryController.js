6eed4167f350699449875fdf02f52d1c89e3f51d
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-16 21:36:21 -0500

    create render for index

--- local-resturant/contollers/categoryController.js
+++ local-resturant/contollers/categoryController.js
@@ -3,12 +3,52 @@ const Desert = require("../models/desert");
 const Drink = require("../models/drink");
 const Entree = require("../models/entree");
 const Side = require("../models/side");
+const DesertInstance = require("../models/desertinstance");
+const EntreeInstance = require("../models/entreeInstance");
+const SideInstance = require("../models/sideinstance");
 
 const asyncHandler = require("express-async-handler");
 
 exports.index = asyncHandler(async (req, res, next) => {
+  const [
+    categories,
+    deserts,
+    drinks,
+    entrees,
+    sides,
+    numDesertInstances,
+    numSideInstances,
+    numEntreeInstances,
+    numAvailableDesertInstance,
+    numAvailableEntreeInstance,
+    numAvailableSideInstance,
+  ] = await Promise.all([
+    Category.find({}, "name").exec(),
+    Desert.find({}, "name price category").sort({ name: 1 }).exec(),
+    Drink.find({}, "name price category").sort({ name: 1 }).exec(),
+    Entree.find({}, "name price category").sort({ name: 1 }).exec(),
+    Side.find({}, "name price category").sort({ name: 1 }).exec(),
+    DesertInstance.countDocuments().exec(),
+    SideInstance.countDocuments({}).exec(),
+    EntreeInstance.countDocuments({}).exec(),
+    DesertInstance.countDocuments({ status: "Available" }).exec(),
+    EntreeInstance.countDocuments({ status: "Available" }).exec(),
+    SideInstance.countDocuments({ status: "Available" }).exec(),
+  ]);
+
   res.render("index", {
     title: "Local Resturant",
+    categories: categories,
+    deserts: deserts,
+    drinks: drinks,
+    entrees: entrees,
+    sides: sides,
+    desert_instance_count: numDesertInstances,
+    side_instance_count: numSideInstances,
+    entree_instance_count: numEntreeInstances,
+    available_desert_instance_count: numAvailableDesertInstance,
+    available_entree_instance_count: numAvailableEntreeInstance,
+    available_side_instance_count: numAvailableSideInstance,
   });
 });
 
