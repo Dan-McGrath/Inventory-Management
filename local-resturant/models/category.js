9bd90aae0c6bcf776bdb34fed751ec6787918911
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-17 22:08:12 -0500

    update schema

--- local-resturant/models/category.js
+++ local-resturant/models/category.js
@@ -3,18 +3,27 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CategorySchema = new Schema({
-  name: { type: String, required: true, maxLength: 100, minLength: 3 },
+  name: {
+    type: String,
+    required: true,
+    maxLength: 100,
+    minLength: 3,
+    lowercase: true,
+  },
 });
 
 CategorySchema.virtual("slug").get(function () {
   let slug = this.name;
-  slug.toLowerCase();
   slug.replace(" ", "_");
   return slug;
 });
 
+CategorySchema.virtual('title').get(function () {
+  return this.name.charAt(0).toUpperCase() + this.name.slice(1);
+})
+
 CategorySchema.virtual("url").get(function () {
-  return `/menu/${this.slug}s`;
+  return `/menu/${this.slug}`;
 });
 
 module.exports = mongoose.model("Category", CategorySchema);
