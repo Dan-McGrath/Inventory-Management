0076ab10b9627f88625c116097f117fd772e854d
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-20 19:56:02 -0500

    create get and post contollers for create instance

--- local-resturant/contollers/iteminstanceController.js
+++ local-resturant/contollers/iteminstanceController.js
@@ -1,4 +1,5 @@
 const ItemInstance = require("../models/iteminstance");
+const Item = require("../models/item");
 
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
@@ -28,13 +29,54 @@ exports.iteminstance_detail = asyncHandler(async (req, res, next) => {
 
 // Display ItemInstance create form on GET.
 exports.iteminstance_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: ItemInstance create GET");
+  const items = await Item.find({}, "title name").exec();
+  console.log(items);
+  res.render("instance_create", {
+    title: "Create Instance",
+    item_list: items,
+  });
 });
 
 // Handle ItemInstance create on POST.
-exports.iteminstance_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: ItemInstance create POST");
-});
+exports.iteminstance_create_post = [
+  (req, res, next) => {
+    if (!(req.body.item instanceof Array)) {
+      if (typeof req.body.item === "undefined") req.body.item = [];
+      else req.body.item = new Array(req.body.item);
+    }
+    next();
+  },
+
+  body("item", "Select Item").trim().isLength({ min: 1 }).escape(),
+  body("made", "Invalid date").isISO8601().toDate(),
+  body("status").escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const itemInstance = new ItemInstance({
+      item: req.body.item,
+      made: req.body.made,
+      status: req.body.status,
+    });
+
+    if (!errors.isEmpty()) {
+      const allItems = await Item.find({}, "name").exec();
+
+      res.render("instance_create", {
+        title: "Create instance",
+        item_list: allItems,
+        selected_item: itemInstance.item._id,
+        errors: errors.array(),
+        iteminstance: itemInstance,
+      });
+      return;
+    } else {
+      await itemInstance.save();
+      res.redirect(itemInstance.url);
+    }
+  }),
+];
 
 // Display ItemInstance delete form on GET.
 exports.iteminstance_delete_get = asyncHandler(async (req, res, next) => {
