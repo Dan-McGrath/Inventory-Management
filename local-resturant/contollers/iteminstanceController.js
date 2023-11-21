851d0f41047408c1be9f394747b5fa602184d77f
Dan McGrath <mcgrathd3254@yahoo.com>
2023-11-21 17:06:23 -0500

    create GET and POST for update

--- local-resturant/contollers/iteminstanceController.js
+++ local-resturant/contollers/iteminstanceController.js
@@ -29,7 +29,7 @@ exports.iteminstance_detail = asyncHandler(async (req, res, next) => {
 // Display ItemInstance create form on GET.
 exports.iteminstance_create_get = asyncHandler(async (req, res, next) => {
   const items = await Item.find({}, "title name").exec();
-  res.render("instance_create", {
+  res.render("instance_form", {
     title: "Create Instance",
     item_list: items,
   });
@@ -61,7 +61,7 @@ exports.iteminstance_create_post = [
     if (!errors.isEmpty()) {
       const allItems = await Item.find({}, "name").exec();
 
-      res.render("instance_create", {
+      res.render("instance_form", {
         title: "Create instance",
         item_list: allItems,
         selected_item: itemInstance.item._id,
@@ -100,10 +100,73 @@ exports.iteminstance_delete_post = asyncHandler(async (req, res, next) => {
 
 // Display ItemInstance update form on GET.
 exports.iteminstance_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: ItemInstance update GET");
+  const [instance, allItems] = await Promise.all([
+    ItemInstance.findById(req.params.id).populate("item").exec(),
+    Item.find().exec(),
+  ]);
+
+  if (instance === null) {
+    const err = new Error("Instance not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  for (const item of allItems) {
+    if (item._id.toString() === instance.item._id.toString()) {
+      item.isSelected = "true";
+    }
+  }
+  res.render("instance_form", {
+    title: "Update Form",
+    item_list: allItems,
+    iteminstance: instance,
+  });
 });
 
 // Handle Iteminstance update on POST.
-exports.iteminstance_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: ItemInstance update POST");
-});
+exports.iteminstance_update_post = [
+  (req, res, next) => {
+    if (!(req.body.item instanceof Array)) {
+      if (typeof req.body.item === undefined) {
+        req.body.item = [];
+      } else {
+        req.body.item = new Array(req.body.item);
+      }
+    }
+    next();
+  },
+  body("item", "Select Item").trim().isLength({ min: 1 }).escape(),
+  body("made", "Invalid date").isISO8601().toDate(),
+  body("status").escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const instance = new ItemInstance({
+      item: req.body.item,
+      made: req.body.made,
+      status: req.body.status,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+      const allItems = await Item.find().exec();
+      console.log(errors.array());
+
+      res.render("instance_form", {
+        title: "Update Instance",
+        item_list: allItems,
+        iteminstance: instance,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      const updateInstance = await ItemInstance.findByIdAndUpdate(
+        req.params.id,
+        instance,
+        {}
+      );
+      res.redirect(updateInstance.url);
+    }
+  }),
+];
